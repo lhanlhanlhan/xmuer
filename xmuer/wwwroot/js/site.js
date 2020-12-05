f226fd099376369db5a3986c4df96f6d5db5d67a
chrishanli <han@han-li.cn>
2020-12-05 22:37:37 +0800

    Project Framework

---
+++ xmuer/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// XMUer 全局 JS
+
+// 导航栏自动选中
+$(function () {
+    $(".han-navbar-list").find("li").each(function () {
+        var a = $(this).find("a:first")[0];
+        if ($(a).attr("href") === location.pathname) {
+            $(this).addClass("active");
+        } else {
+            $(this).removeClass("active");
+        }
+    });
+})
+
+// 左侧导航栏 active
