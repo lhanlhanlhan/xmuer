fcb3b40a4a3d5dcd8be329ef9b4f394d46fd8a5f
lhanlhanlhan <han@han-li.cn>
2020-12-22 20:02:20 +0800

    个人中心及CSS魔改

--- xmuer/Pages/Homepage/UserHome.cshtml.cs
+++ xmuer/Pages/Homepage/UserHome.cshtml.cs
@@ -24,11 +24,15 @@ namespace xmuer.Pages.Homepage
 
         public string WelcomeMessage { get; set; }
         public string StudySchool { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
         public string imgUrl { get; set; }
+        public string Description { get; set; }
+        public List<string> Comments { get; set; }
 
         public IActionResult OnGet()
         {
-            // �ж��Ƿ��������˷���
+            // 获取用户 ID
             int id = -1;
             IEnumerator<KeyValuePair<string, StringValues>> enumerator = HttpContext.Request.Query.GetEnumerator();
             while (enumerator.MoveNext())
@@ -38,17 +42,21 @@ namespace xmuer.Pages.Homepage
             }
             if (id > 0)
             {
-                // ��ȡ��ǰ��¼���û�
+                // 获取用户个人资料
                 user = _db.Users.Find(id);
                 if (user != null)
                 {
                     WelcomeMessage = user.userName;
-                    StudySchool = user.university != null ? user.university : "...";
+                    StudySchool = user.university != null ? user.university : "未知的学校";
+                    Email = user.email;
                     imgUrl = user.Avatar;
+                    Department = user.Department;
+                    Description = "你太懒了，还没有写个人简介哦！"; // TODO
+                    Comments = new List<string>(); // TODO
                     return Page();
                 }
             }
-            // ��ȡ��ǰ��¼���û�
+            // 获取用户 ID
             string tmp = HttpContext.Session.GetString("userId");
             if(tmp == "" || tmp == null)
             {
@@ -57,8 +65,12 @@ namespace xmuer.Pages.Homepage
             userId = Convert.ToInt32(tmp);
             user = _db.Users.Find(userId);
             WelcomeMessage = user.userName;
-            StudySchool = user.university != null ? user.university : "...";
+            StudySchool = user.university != null ? user.university : "未知的学校";
+            Email = user.email;
             imgUrl = user.Avatar;
+            Department = user.Department;
+            Description = "你太懒了，还没有写个人简介哦！"; // TODO
+            Comments = new List<string>(); // TODO
             return Page();
         }
     }
